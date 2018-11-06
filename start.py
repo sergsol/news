1a3196bc58d3d85fa8ab4a0853a19ecc9542f438
sergsol <serge.soloshchenko@gmail.com>
2018-11-06 16:37:28 +0100

    added port

--- start.py
+++ start.py
@@ -10,5 +10,6 @@ BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 def get_news():
     return render_template('index.html')
 
+
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=9999)
