bcfdb3fefbee50a010cd2ccfd2e510d7286b6c28
sergsol <serge.soloshchenko@gmail.com>
2018-11-06 08:31:40 +0100

    start

---
+++ start.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import feedparser
+
+
+app = Flask(__name__)
+BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+
+
+@app.route('/')
+def get_news():
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
