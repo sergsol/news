c55945f30889327eef0312e14def0b5d10cd2a17
sergsol <serge.soloshchenko@gmail.com>
2018-11-11 22:06:53 +0100

    fefactored get article updated index.html

--- start.py
+++ start.py
@@ -3,39 +3,18 @@ import feedparser
 
 
 app = Flask(__name__)
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+
 feeds = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
          'cnn': 'http://rss.cnn.com/rss/edition.rss',
-         'fox': 'http://feeds.foxnews.com/foxnews/latest',
-         'IOL': 'http://www.iol.co.za/cmlink/1.640'}
+         'fox': 'http://feeds.foxnews.com/foxnews/latest',}
 
-@app.route("/")
-@app.route("/bbc")
-def bbc():
-    # return render_template('index.html')
-    return get_news('bbc')
-
-@app.route("/cnn")
-def cnn():
-    return get_news('cnn')
 
 @app.route("/")
 @app.route("/<publication>")
 def get_news(publication="bbc"):
     feed = feedparser.parse(feeds[publication])
-    first_article = feed['entries'][0]
-    return """
-    <body>
-    <h1> Headlines </h1>
-    <b>{}</b>
-    <br>
-    <i>{}</i>
-    <p>{}</p>
-    </body>
-    """.format(first_article.get('title'),
-               first_article.get('published'),
-               first_article.get('summary'),
-               )
+    return render_template('index.html',
+                           articles=feed['entries'])
 
 
 if __name__=="__main__":
