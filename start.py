2f62c048e5e46258b3ce811c02d6ca8783068740
serge.soloshchenko@gmail.com <Soloxa877!>
2019-03-15 06:26:06 +0100

    Added weaher input

--- start.py
+++ start.py
@@ -15,27 +15,41 @@ RSS_FEEDS = {'bbc': 'http://RSS_FEEDS.bbci.co.uk/news/rss.xml',
              'fox': 'http://RSS_FEEDS.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
+DEFAULTS = {'publication': 'cnn',
+            'city': 'London,UK'}
+
 
 @app.route("/", methods=['GET', 'POST'])
-def get_news():
-    query = request.form.get('publication')
+def home():
+    publication = request.form.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    city = request.form.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    return render_template("index.html", articles=articles, weather=weather)
+
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
+        publication = DEFAULTS['publication']
     else:
         publication = query.lower()
-    feed = feedparser.parse(RSS_FEEDS[publication])
-    weather = get_weather(location='London,UK')
-    return render_template("index.html", articles=feed['entries'], weather=weather)
+    publication = feedparser.parse(RSS_FEEDS[publication])
+    return publication['entries']
 
-
-def get_weather(location="Kyiv"):
+def get_weather(city):
     API_KEY = '2ed83ec7593c5df3c352a7c0870e699d'
-    api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'.format(location, API_KEY)
+    api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'.format(city, API_KEY)
     response = requests.get(api_url).json()
-    print(response)
+    print(response['sys']['country'])
     return response
 
-get_weather()
+
+
+
 
 
 if __name__ == "__main__":
