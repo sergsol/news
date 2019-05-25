c086c50471f81abee9d873c0209ff82599b99085
Soloshchenko Serhii <serhii.soloshchenko@trodat.net>
2019-05-25 08:11:12 +0200

    Currency added

--- start.py
+++ start.py
@@ -15,13 +15,18 @@ RSS_FEEDS = {'bbc': 'http://RSS_FEEDS.bbci.co.uk/news/rss.xml',
              'fox': 'http://RSS_FEEDS.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
+RATES = 'https://openexchangerates.org/api/latest.json?app_id=24ea695da8fc45148ea0d8d6ec60ec8f'
+WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=2ed83ec7593c5df3c352a7c0870e699d'
 DEFAULTS = {'publication': 'cnn',
-            'city': 'London,UK'}
+            'city': 'London,UK',
+            'currency_from': 'GBP',
+            'currency_to': 'UAH'}
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     publication = request.form.get('publication')
+    city = request.form.get('city')
     if not publication:
         publication = DEFAULTS['publication']
     articles = get_news(publication)
@@ -29,7 +34,15 @@ def home():
     if not city:
         city = DEFAULTS['city']
     weather = get_weather(city)
-    return render_template("index.html", articles=articles, weather=weather)
+    currency_from = request.form.get('currency_from')
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.form.get('currency_to')
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rete, currencies = get_rates(currency_from, currency_to)
+    return render_template("index.html", articles=articles, weather=weather, rates=rete,
+                           currency_from=currency_from, currency_to=currency_to, currensies=currencies)
 
 
 def get_news(query):
@@ -40,12 +53,25 @@ def get_news(query):
     publication = feedparser.parse(RSS_FEEDS[publication])
     return publication['entries']
 
+
 def get_weather(city):
-    API_KEY = '2ed83ec7593c5df3c352a7c0870e699d'
-    api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'.format(city, API_KEY)
-    response = requests.get(api_url).json()
-    print(response['sys']['country'])
-    return response
+    response = requests.get(WEATHER_URL.format(city)).json()
+    weather = {
+                'description': response['weather'][0]['description'],
+                'temperature': response['main']['temp'],
+                'country': response['sys']['country'],
+                'city': response['name']
+                }
+    return weather
+
+
+def get_rates(frm, to):
+    rates = requests.get(RATES).json()
+    frm_rate = rates['rates'][frm.upper()]
+    to_rate = rates['rates'][to.upper()]
+    return to_rate/frm_rate, rates['rates'].keys()
+
+
 
 
 
@@ -53,4 +79,4 @@ def get_weather(city):
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(port=5001, debug=True)
